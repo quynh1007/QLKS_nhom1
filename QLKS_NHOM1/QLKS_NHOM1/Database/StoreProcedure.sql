5b903dbb92dbab09b8aa3a604ae05d6423cf1372
dat78787878 <d782000@gmail.com>
2021-04-14 16:01:37 +0700

    update database and create fPhong

--- QLKS_NHOM1/QLKS_NHOM1/Database/StoreProcedure.sql
+++ QLKS_NHOM1/QLKS_NHOM1/Database/StoreProcedure.sql
@@ -1,1 +1,82 @@
-use QL_KS
+
+---phong--
+
+CREATE PROCEDURE SP_PHONG_GetAll
+AS
+BEGIN
+  SELECT *
+  FROM PHONG
+END
+GO
+
+CREATE PROCEDURE SP_PHONG_Insert
+  @TenPhong NVARCHAR(50),
+  @LoaiPhong NVARCHAR(10),
+  @GiaPhong int,
+  @ChuThich NVARCHAR(50),
+  @TinhTrang NVARCHAR(10),
+  @MaNV int,
+  @MaDichVu int
+AS
+BEGIN
+  INSERT INTO PHONG
+    (TenPhong, LoaiPhong, GiaPhong, ChuThich ,TinhTrang , MaNV , MaDichVu )
+  VALUES(@TenPhong, @LoaiPhong, @GiaPhong, @ChuThich , @TinhTrang ,@MaNV , @MaDichVu)
+END
+GO
+
+CREATE PROCEDURE SP_PHONG_Delete
+  @MaPhong INT
+AS
+BEGIN
+  UPDATE HOADON
+  SET MaPhong = NULL
+  WHERE MaPhong = @MaPhong
+
+  UPDATE KHACHHANG
+  SET MaPhong = NULL
+  WHERE MaPhong = @MaPhong
+
+  DELETE PHONG
+  WHERE MaPhong = @MaPhong
+END
+GO
+
+CREATE PROCEDURE SP_PHONG_Update
+  @MaPhong int ,
+  @TenPhong NVARCHAR(50),
+  @LoaiPhong NVARCHAR(10),
+  @GiaPhong int,
+  @ChuThich NVARCHAR(50),
+  @TinhTrang NVARCHAR(10),
+  @MaNV int,
+  @MaDichVu int
+AS
+BEGIN
+  UPDATE PHONG
+  SET TenPhong = @TenPhong,
+  LoaiPhong = @LoaiPhong,
+  GiaPhong = @GiaPhong,
+  TinhTrang = @TinhTrang,
+  MaNV = @MaNV ,
+  MaDichVu = @MaDichVu 
+  WHERE MaPhong = @MaPhong
+END
+GO
+
+CREATE PROCEDURE SP_PHONG_Search
+  @searchValue NVARCHAR(200)
+AS
+BEGIN
+  SELECT *
+  FROM PHONG
+  WHERE MaPhong LIKE N'%' + @searchValue + '%'
+    OR MaPhong LIKE N'%' + @searchValue + '%'
+    OR TenPhong LIKE N'%' + @searchValue + '%'
+    OR LoaiPhong LIKE N'%' + @searchValue + '%'
+    OR GiaPhong LIKE N'%' + @searchValue + '%'
+    OR TinhTrang LIKE N'%' + @searchValue + '%'
+	OR MaNV LIKE N'%' + @searchValue + '%'
+    OR MaDichVu LIKE N'%' + @searchValue + '%'
+END
+GO
---
+++ QLKS_NHOM1/QLKS_NHOM1/GUI/fPhong.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace QLKS_NHOM1.GUI
+{
+    partial class fPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ma = new System.Windows.Forms.Label();
+            this.txtMaPhong = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ma
+            // 
+            this.Ma.AutoSize = true;
+            this.Ma.Location = new System.Drawing.Point(66, 45);
+            this.Ma.Name = "Ma";
+            this.Ma.Size = new System.Drawing.Size(63, 15);
+            this.Ma.TabIndex = 0;
+            this.Ma.Text = "Mã phòng";
+            // 
+            // txtMaPhong
+            // 
+            this.txtMaPhong.AutoSize = true;
+            this.txtMaPhong.Location = new System.Drawing.Point(132, 45);
+            this.txtMaPhong.Name = "txtMaPhong";
+            this.txtMaPhong.Size = new System.Drawing.Size(0, 15);
+            this.txtMaPhong.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giá";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(264, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tên phòng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(496, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Loại phòng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(264, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Chú thích";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(496, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Tình trạng";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(70, 159);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Mã nhân viên";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(373, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Mã dịch vụ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(336, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(579, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(121, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(116, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(337, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(116, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(579, 91);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(116, 22);
+            this.textBox5.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(161, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 23);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(475, 156);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(140, 23);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(70, 247);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(664, 247);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(379, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 22);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Phòng";
+            // 
+            // fPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 559);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMaPhong);
+            this.Controls.Add(this.Ma);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "fPhong";
+            this.Text = "fPhong";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Ma;
+        private System.Windows.Forms.Label txtMaPhong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
